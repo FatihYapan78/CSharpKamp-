e941a5d6c07c47b3be9fecc327d976a623ac84f6
Fatih Yapan <yapanfatih78@gmail.com>
2024-10-02 12:21:47 +0300

    .

---
+++ CSharpEgitimKampi/01_MainSubjects/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Yazdırma Komutları
+            #region Yazdırma Komutları
+            //Console.Write("Merhaba DÜnya");
+            //Console.WriteLine("Selam");
+
+            //Console.WriteLine("***** Yemek Kategorileri *****");
+            //Console.WriteLine();
+            //Console.WriteLine("1- Çorbalar");
+            //Console.WriteLine("2- Ana Yemekler");
+            //Console.WriteLine("3- Soğuk Başlangıçlar");
+            //Console.WriteLine("4- Salatalar");
+            //Console.WriteLine("5- Tatlılar");
+            //Console.WriteLine("6- İçecekler");
+            //Console.WriteLine();
+            //Console.WriteLine("***** Yemek Kategorileri *****");
+
+            #endregion
+
+            // Değişkenler 
+            #region Değiskenler
+
+            // string 
+            // Değişken_türü Değişken_adı
+
+            //string name;
+            //name = "Fatih";
+            //Console.WriteLine(name);
+
+            string customerName;
+            string customerSurName;
+            string customerPhone;
+            string customerEmail, district, city;
+
+            customerName = "Fatih";
+            customerSurName = "Yapan";
+            customerPhone = "507 963 71 95";
+            customerEmail = "yapanfatih78@gmail.com";
+            district = "Kadıköy";
+            city = "İstanbul";
+
+            Console.WriteLine(customerName);
+            Console.WriteLine(customerSurName);
+            Console.WriteLine(customerPhone);
+            Console.WriteLine(customerEmail);
+            Console.WriteLine(city);
+            Console.WriteLine(district);
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
+
+
+
+
---
+++ CSharpEgitimKampi/02_Variables/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region DoubleDegiskenler
+            double number;
+            number = 4.85; // Noktanın çıktısı virgül şeklinde oluyor.
+            Console.WriteLine(number);
+
+            #endregion
+
+            #region CharDegiskenler
+
+            // Tek tırnak kullanılarak tanımlama yapılır.
+
+            char symbol;
+            symbol = 'a';
+            Console.WriteLine(symbol);
+
+
+
+            #endregion
+
+            #region KlavyedenVeriGirisleriStrigDegiskenler
+
+            //Console.WriteLine("Yolcu Bilgieri");
+            //Console.WriteLine();
+
+            //string name, surname, district, city, age, IdentityNumber;
+
+            //Console.Write("Yolcu Adı: ");
+            //name = Console.ReadLine();
+
+            //Console.Write("Yolcu SoyAdı: ");
+            //surname = Console.ReadLine();
+
+            //Console.WriteLine(name + " " + surname);
+
+            #endregion
+
+            #region KlavyedenTamSayıGirisleriveDönüsümler
+
+            //int shoesPrice;
+            //shoesPrice = 1000;
+
+            //int shoesCount;
+
+            //Console.Write("Kaç Adet Ayakkabı Alacaksınız : ");
+            //shoesCount = int.Parse(Console.ReadLine());
+
+            //int TotalPrice = shoesPrice * shoesCount;  
+            //Console.WriteLine(TotalPrice);
+
+            #endregion
+
+            #region KlavyedenOndalıklıSayıGirisleriveDönüsümler
+
+            double exam1, result;
+
+            Console.Write("Lütfen Sınav Notunu Gİriniz : ");
+            exam1 = double.Parse(Console.ReadLine());
+
+            #endregion
+
+            #region KlavyedenKarakterGirisleriveDönüsümler
+
+            char gender;
+            Console.Write("Lütfen Cinsiyet Seçiniz : ");
+            gender = char.Parse(Console.ReadLine());
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
